ba85af3cabc10fcc4a3a00150a8a92772c1400d6
Majo <a20181563@pucp.edu.pe>
2020-11-08 23:09:28 -0500

    lista de bodegas_2

--- src/main/java/daos/ClienteD.java
+++ src/main/java/daos/ClienteD.java
@@ -14,7 +14,7 @@ public class ClienteD {
     public ClienteD() {
     }
 
-    public int calcularCantPag() {
+    public int calcularCantPag(){
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
@@ -22,171 +22,57 @@ public class ClienteD {
         }
 
         String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
-        String sql = "select ceil(count(nombreFoto)/8) from bodega ;";
+
+        String sql = "select ceil(count(nombreFoto)/8)\n" +
+                "from bodega\n;";
+
         int cantPag = 0;
+        try (Connection conn = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql);) {
 
-        try {
-            Connection conn = DriverManager.getConnection(url, "root", "root");
-            Throwable var5 = null;
+            rs.next();
+            cantPag = rs.getInt(1);
 
-            try {
-                Statement stmt = conn.createStatement();
-                Throwable var7 = null;
-
-                try {
-                    ResultSet rs = stmt.executeQuery(sql);
-                    Throwable var9 = null;
-
-                    try {
-                        rs.next();
-                        cantPag = rs.getInt(1);
-                    } catch (Throwable var59) {
-                        var9 = var59;
-                        throw var59;
-                    } finally {
-                        if (rs != null) {
-                            if (var9 != null) {
-                                try {
-                                    rs.close();
-                                } catch (Throwable var58) {
-                                    var9.addSuppressed(var58);
-                                }
-                            } else {
-                                rs.close();
-                            }
-                        }
-
-                    }
-                } catch (Throwable var62) {
-                    var7 = var62;
-                    throw var62;
-                } finally {
-                    if (stmt != null) {
-                        if (var7 != null) {
-                            try {
-                                stmt.close();
-                            } catch (Throwable var57) {
-                                var7.addSuppressed(var57);
-                            }
-                        } else {
-                            stmt.close();
-                        }
-                    }
-
-                }
-            } catch (Throwable var64) {
-                var5 = var64;
-                throw var64;
-            } finally {
-                if (conn != null) {
-                    if (var5 != null) {
-                        try {
-                            conn.close();
-                        } catch (Throwable var56) {
-                            var5.addSuppressed(var56);
-                        }
-                    } else {
-                        conn.close();
-                    }
-                }
-
-            }
-        } catch (SQLException var66) {
-            var66.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
-
         return cantPag;
     }
 
-    public ArrayList<distritosB> listarBodegas(int pagina) {
+    public ArrayList<distritosB> listarBodegas(int pagina){
+
         ArrayList listaBodegas = new ArrayList();
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-        } catch (ClassNotFoundException e2) {
-            e2.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
 
         String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
-        int limit = (pagina - 1) * 8;
+
+        int limit = (pagina-1)*8;
+
         String sql = "select nombreFoto,rutaFoto ,nombreBodega, direccion\n" +
                 "from bodega where lower(estado) = \"activo\" limit ?,8;;";
 
-        try {
-            Connection conn = DriverManager.getConnection(url, "root", "root");
-            Throwable var7 = null;
+        try (Connection conn = DriverManager.getConnection(url, "root", "root");
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
 
-            try {
-                PreparedStatement pstmt = conn.prepareStatement(sql);
-                Throwable var9 = null;
+            pstmt.setInt(1, limit);
 
-                try {
-                    pstmt.setInt(1, limit);
-                    ResultSet rs = pstmt.executeQuery();
-                    Throwable var11 = null;
-
-                    try {
-                        while(rs.next()) {
-                            distritosB bodega = new distritosB();
-                            bodega.setNombreFoto(rs.getString(1));
-                            bodega.setRutaFoto(rs.getString(2));
-                            bodega.setNombreBodega(rs.getString(3));
-                            bodega.setDireccion(rs.getBigDecimal(4));
-                            listaBodegas.add(bodega);
-                        }
-                    } catch (Throwable var62) {
-                        var11 = var62;
-                        throw var62;
-                    } finally {
-                        if (rs != null) {
-                            if (var11 != null) {
-                                try {
-                                    rs.close();
-                                } catch (Throwable var60) {
-                                    var11.addSuppressed(var60);
-                                }
-                            } else {
-                                rs.close();
-                            }
-                        }
-
-                    }
-                } catch (Throwable var64) {
-                    var9 = var64;
-                    throw var64;
-                } finally {
-                    if (pstmt != null) {
-                        if (var9 != null) {
-                            try {
-                                pstmt.close();
-                            } catch (Throwable var59) {
-                                var9.addSuppressed(var59);
-                            }
-                        } else {
-                            pstmt.close();
-                        }
-                    }
-
+            try(ResultSet rs = pstmt.executeQuery();){
+                while (rs.next()) {
+                    distritosB bodega = new distritosB();
+                    bodega.setNombreFoto(rs.getString(1));
+                    bodega.setRutaFoto(rs.getString(2));
+                    bodega.setNombreBodega(rs.getString(3));
+                    bodega.setDireccion(rs.getString(4));
+                    listaBodegas.add(bodega);
                 }
-            } catch (Throwable var66) {
-                var7 = var66;
-                throw var66;
-            } finally {
-                if (conn != null) {
-                    if (var7 != null) {
-                        try {
-                            conn.close();
-                        } catch (Throwable var58) {
-                            var7.addSuppressed(var58);
-                        }
-                    } else {
-                        conn.close();
-                    }
-                }
-
             }
-        } catch (SQLException var68) {
-            var68.printStackTrace();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
 
         return listaBodegas;
