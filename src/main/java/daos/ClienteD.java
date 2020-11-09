fb941ff51cb577f556b7df965452dd19ca0f5afe
Emerson Munoz <munoz.e@pucp.edu.pe>
2020-11-09 04:32:13 -0500

    listar bodega por distrito con 2 clases mas a ClienteD

--- src/main/java/daos/ClienteD.java
+++ src/main/java/daos/ClienteD.java
@@ -77,4 +77,71 @@ public class ClienteD {
 
         return listaBodegas;
     }
+
+    public int calcularCantPagDistrito(int idDistrito){
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
+
+        String sql = "select ceil(count(nombreFoto)/3)\n" +
+                "from bodega\n where idDistrito="+idDistrito+";";
+
+        int cantPag = 0;
+        try (Connection conn = DriverManager.getConnection(url, "root", "root");
+             Statement stmt = conn.createStatement(); ResultSet rs = stmt.executeQuery(sql);) {
+
+            rs.next();
+            cantPag = rs.getInt(1);
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return cantPag;
+    }
+
+
+    public ArrayList<distritosB> listarBodegasDistrito(int pagina, int idDistrito){
+        ArrayList<distritosB> listaBodegas = new ArrayList();
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        String url = "jdbc:mysql://localhost:3306/mydb?serverTimezone=America/Lima";
+
+        int limit = (pagina-1)*3;
+
+        String sql = "select nombreFoto,rutaFoto ,nombreBodega, direccion\n" +
+                "from bodega where idDistrito = "+idDistrito+" " +
+                "and  lower(estado) = \"activo\" limit ?,3;;";
+
+
+        try (Connection conn = DriverManager.getConnection(url, "root", "root");
+             PreparedStatement pstmt = conn.prepareStatement(sql);) {
+
+            pstmt.setInt(1, limit);
+
+            try(ResultSet rs = pstmt.executeQuery();){
+                while (rs.next()) {
+                    distritosB bodega = new distritosB();
+                    bodega.setNombreFoto(rs.getString(1));
+                    bodega.setRutaFoto(rs.getString(2));
+                    bodega.setNombreBodega(rs.getString(3));
+                    bodega.setDireccion(rs.getString(4));
+                    listaBodegas.add(bodega);
+                }
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+
+        return listaBodegas;
+
+    }
 }
---
+++ src/main/java/servlets/BodegaDistritoServlet.java
@@ -0,0 +1,53 @@
+package servlets;
+
+import beans.distritosB;
+import daos.ClienteD;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+
+@WebServlet(name = "BodegaDistritoServlet", urlPatterns = "/BodegaDistritoServlet")
+public class BodegaDistritoServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        ClienteD clientDao = new ClienteD();
+        int idCodigo = 1;
+
+        String pag = request.getParameter("pag") == null ? "1" : request.getParameter("pag");
+
+        int cantPag = clientDao.calcularCantPagDistrito(idCodigo);
+
+        int paginaAct;
+        try{
+            paginaAct = Integer.parseInt(pag); //try
+            if(paginaAct>cantPag){
+                paginaAct = 1;
+            }
+        }catch(NumberFormatException e){
+            paginaAct = 1;
+        }
+
+
+        ArrayList<distritosB> listaBodegas = clientDao.listarBodegasDistrito(paginaAct,idCodigo);
+
+        request.setAttribute("listaBodegas", listaBodegas);
+        request.setAttribute("cantPag", cantPag);
+        request.setAttribute("paginaAct", paginaAct);
+
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("listaBodegaDistrito.jsp");
+        requestDispatcher.forward(request, response);
+    }
+}
+
